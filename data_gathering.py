843fd09c8b95357a3db9b514883263be7a3e65ab
aniket132123 <singani17@outlook.com>
2026-02-17 23:29:46 -0500

    update weather lambda
    - made some updates to the weather lambda
    - need to use geocoding api to convert city locations into latitude and longitude

--- data_gathering.py
+++ data_gathering.py
@@ -4,68 +4,73 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import pandas as pd
+import datetime
 
 # load .env
-load_dotenv()
+# load_dotenv()
 
-# Connect to postgres server and create table
-engine = create_engine("postgresql+psycopg2://ds_user:mypassword@localhost:5432/cost_of_living_data")  
+# # Connect to postgres server and create table
+# engine = create_engine("postgresql+psycopg2://ds_user:mypassword@localhost:5432/cost_of_living_data")  
 
-# download mean housing sale prices since 2008
-zillow_url = 'https://files.zillowstatic.com/research/public_csvs/mean_sale_price_now/Metro_mean_sale_price_now_uc_sfrcondo_month.csv?t=1767743583'
-df = pd.read_csv(zillow_url)
+# # download mean housing sale prices since 2008
+# zillow_url = 'https://files.zillowstatic.com/research/public_csvs/mean_sale_price_now/Metro_mean_sale_price_now_uc_sfrcondo_month.csv?t=1767743583'
+# df = pd.read_csv(zillow_url)
 
-# drop the mean national sales data for now
-df = df.drop(index=0, axis=0)
+# # drop the mean national sales data for now
+# df = df.drop(index=0, axis=0)
 
-# take mean sales from 2025
-house_prices = df[['RegionName', '2025-01-31', '2025-02-28', '2025-03-31', '2025-04-30', 
-                   '2025-05-31', '2025-06-30', '2025-07-31', '2025-08-31', 
-                   '2025-09-30', '2025-10-31', '2025-11-30']]
+# # take mean sales from 2025
+# house_prices = df[['RegionName', '2025-01-31', '2025-02-28', '2025-03-31', '2025-04-30', 
+#                    '2025-05-31', '2025-06-30', '2025-07-31', '2025-08-31', 
+#                    '2025-09-30', '2025-10-31', '2025-11-30']]
 
 
 
-house_prices.to_sql(
-    'house_prices',
-    engine,
-    if_exists="replace",
-    index=False
-)
+# house_prices.to_sql(
+#     'house_prices',
+#     engine,
+#     if_exists="replace",
+#     index=False
+# )
 
-# Salaries API
-endpoint = f"/v1/comparesalaries/{os.getenv("SALARIES_USER_ID")}/wageocc"
-url = "https://api.careeronestop.org" + endpoint
+# # Salaries API
+# endpoint = f"/v1/comparesalaries/{os.getenv("SALARIES_USER_ID")}/wageocc"
+# url = "https://api.careeronestop.org" + endpoint
 
-headers = {
-    "Content-Type":"application/json",
-    "Authorization": f"Bearer {os.getenv("SALARIES_API_KEY")}"
-}
+# headers = {
+#     "Content-Type":"application/json",
+#     "Authorization": f"Bearer {os.getenv("SALARIES_API_KEY")}"
+# }
 
-# TODO: get the salaries for every state in housing prices table
-# TODO: get the salaries for various different positions
-params = {
-    "keyword" : "Software Developers",
-    "location" : "VA",
-    "enableMetaData" : False
-}
+# # TODO: get the salaries for every state in housing prices table
+# # TODO: get the salaries for various different positions
+# params = {
+#     "keyword" : "Software Developers",
+#     "location" : "VA",
+#     "enableMetaData" : False
+# }
 
-response = requests.get(url, headers=headers, params=params)
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Error: {response.status_code}")
+# response = requests.get(url, headers=headers, params=params)
+# if response.status_code == 200:
+#     data = response.json()
+#     print(data)
+# else:
+#     print(f"Error: {response.status_code}")
 
 
 # OPEN-METEO API
 # TODO: run through all the cities in housing and gather weather data
+curr_date = datetime.datetime.now().date()
+start_date = curr_date - datetime.timedelta(days=90)
+end_date = curr_date + datetime.timedelta(days=16)
+
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
     "latitude": '47.6',
     "longitude": '122.3',
-    "start_date": "2025-10-24",
-    "end_date": "2026-01-22",
-    "daily": "temperature_2m_mean"
+    "start_date": start_date.strftime("%Y-%m-%d"),
+    "end_date": end_date.strftime("%Y-%m-%d"),
+    "daily": ["temperature_2m_mean", "snowfall_sum", "rain_sum", "wind_speed_10m_max", "daylight_duration"]
 }
 response = requests.get(url, params=params)
 print(response.json())
--- ingestion_lambda/weather_lambda.py
+++ ingestion_lambda/weather_lambda.py
@@ -1,8 +1,44 @@
 import json
+import datetime
+import boto3
+import urllib3
+import pandas as pd
+import requests
 
 def lambda_handler(event, context):
-    # TODO implement
+    curr_date = datetime.datetime.now().date()
+    start_date = curr_date - datetime.timedelta(days=90)
+    end_date = curr_date + datetime.timedelta(days=16)
+
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": '47.6',
+        "longitude": '122.3',
+        "start_date": start_date.strftime("%Y-%m-%d"),
+        "end_date": end_date.strftime("%Y-%m-%d"),
+        "daily": "temperature_2m_mean"
+    }
+
+    try:
+        http = urllib3.PoolManager()
+        r = http.request('GET', url, fields=params)
+
+        # Convert to Pandas DataFrame
+        df = pd.DataFrame(data['daily'])
+
+        # Upload to S3
+        csv_buffer = StringIO()
+        df.to_csv(csv_buffer, index=False)
+        s3_resource = boto3.resource('s3')
+        s3_resource.Object('weather-data-2023', f'weather_data_{curr_date}.csv').put(Body=csv_buffer.getvalue())
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Failed to upload file to S3')
+        }
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Upload to S3 successful!')
     }
